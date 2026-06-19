980ce3a67249ae1499394c38dbb82bdeb9c8f562
Harshil6937 <vadgashiyaharshil@gmail.com>
2026-06-19 12:57:25 +0530

    Delete OS IMP SHELL SCRIPT directory

--- OS IMP SHELL SCRIPT/odd-even.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter Number"
-read a
-
-if [ `expr $a % 2` -eq 0 ]
-then
-	echo "Even Number"
-else
-	echo "Odd Number"
-fi
--- OS IMP SHELL SCRIPT/positive.sh
+++
@@ -1,9 +0,0 @@
-echo "enter number"
-read a
-
-if [ $a -gt 0 ]
-then
-	echo "Positive Number"
-else
-	echo "Negative Number"
-fi
